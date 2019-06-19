bce249e135d61562eb5f46fc0d989b3436cfe8d9
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-19 16:25:34 +0200

    In the beginning, there was darkness

---
+++ .i3/status.py
@@ -0,0 +1,72 @@
+################################
+# i3pystatus configuration file.
+################################
+from i3pystatus import Status
+import os
+import colors
+
+# Get status bar
+status = Status(standalone=True)
+
+##############
+#color palette
+##############
+color_good = colors.base05
+color_warn = colors.base09
+color_bad  = colors.base08
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%a %-d %b %T ",)
+
+status.register("cpu_usage")
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+net_iface = "wlp2s0"
+net_fmt=" {essid:.24}"
+
+status.register("network",
+    interface=net_iface,
+    dynamic_color=False,
+    on_leftclick="wicd-gtk",
+    format_up=net_fmt,
+    color_down=color_bad,
+    start_color=color_good,
+    end_color=color_bad)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    color_muted=color_warn,
+    color_unmuted=color_good,
+    format="  {volume:>03}",
+    format_muted="     ")
+
+
+# show backlight brightness status
+backlight='acpi_video0'
+if os.path.exists('/sys/class/backlight/{}'.format(backlight)):
+    status.register("backlight",
+            backlight=backlight,
+            color=color_good)
+
+# show battery status
+status.register("battery",
+        full_color=color_good,
+        format="  {percentage:>3.0f}%",
+        alert=True,
+        alert_percentage=20,
+        critical_color=color_bad,
+        not_present_text="",
+        charging_color=color_good)
+
+# run the thing!
+status.run()
